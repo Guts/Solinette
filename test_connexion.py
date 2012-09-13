28a9c7ce1eff26b63cec87b6b6662a454b5c2384
Julien <julien.moura@gmail.com>
2012-09-13 12:57:17 -0500

    script de test

---
+++ test_connexion.py
@@ -0,0 +1,15 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Utilisateur
+#
+# Created:     04/09/2012
+# Copyright:   (c) Utilisateur 2012
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+from Tkinter import *
+import GUI_Solinette
+
