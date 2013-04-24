2f52b5d819438603817d222b6d156ec2d7a95ed6
Julien <julien.moura@gmail.com>
2013-04-24 12:12:21 -0500

    WIP: automatization of compilation

--- compilation/setup_solinette.py
+++ compilation/setup_solinette.py
@@ -1,40 +1,42 @@
 ﻿#!/usr/bin/python
 #-*- coding: utf-8 -*-
+
+### importation
+# standard library
 from distutils.core import setup
-import py2exe
 import os
 import sys
 import subprocess
 import shutil
 from glob import glob
-import modulefinder
-from ..modules import Solinette_main
-##mfcdir = r'C:\Python27\Lib\site-packages\pythonwin'
-##mfcfiles = [os.path.join(mfcdir, i) for i in ["mfc90.dll", "mfc90u.dll", "mfcm90.dll", "mfcm90u.dll", "Microsoft.VC90.MFC.manifest"]]
 
-includes = ["encodings",
-            "encodings.utf_8",
-            ]
+# third-party library
+import py2exe
 
-packages = r'C:\Documents and Settings\Utilisateur\Mes documents\GitHub\Solinette\modules\SolinetteGUI'
+# custom modules
+from modules import *
 
+# options
 py2exe_options = dict(
-                      packages = packages,
                       compressed=True,  # Compress library.zip
-                      optimize = 2
+                      optimize = 2,
+                      dist_dir = 'Solinette'
                       )
+# setup settings
 setup(
     name="Solinette",
     version="1.5.1",
     description=u"Geocoding for Lima and Callao",
     author="Julien Moura et Pierre Vernier",
+    author_mail = "julien.moura@gmail.com",
+    url = "https://github.com/Guts/Solinette",
     license="license GPL v3.0",
-    data_files = [("documentacion",["../documentation/Instalacion_PostGIS_UsoSolinette_ES.docx"])],
+    data_files = [("", ["icone_Solinette.ico"]),("sources", ["sources/logo_Solinette.GIF"]),("documentacion",["documentation/Instalacion_PostGIS_UsoSolinette_ES.docx"])],
     options={'py2exe': py2exe_options},
     windows = [
         {
-            "script": "../Solinette.py",                    ### Main Python script
-            "icon_resources": [(1, "../icone_Solinette.ico")]     ### Icon to embed into the PE file.
+            "script": "Solinette.py",                    ### Main Python script
+            "icon_resources": [(1, "icone_Solinette.ico")]     ### Icon to embed into the PE file.
         }
               ],
     )
